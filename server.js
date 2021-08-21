a0687feb2eca5fc82cdda7cc1d3504eab8822f8d
naveed331 <naveed.ashfaq@graana.com>
2021-08-21 15:39:16 +0500

    Setup .env

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const path = require("path");
 const helmet = require("helmet");
 const express = require("express");
 
+require("dotenv").config();
+
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
 const app = express();
 app.use(helmet());
