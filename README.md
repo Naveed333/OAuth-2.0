bb57941a0bcaaf697e92e2b8c48f15b87ddb3f24
naveed331 <naveed.ashfaq@graana.com>
2021-08-21 13:13:23 +0500

    Openssl Commands

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # OAuth-2.0
+
+Use OpenSSl to created that certificate
+
+$ openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
+
+in common Name : should be domain name
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const https = require("https");
 const path = require("path");
 const express = require("express");
 
@@ -13,6 +15,17 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}...`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Listening on PORT ${PORT}...`);
+// });
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}...`);
+  });
